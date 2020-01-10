ac44dab1e50f9d18798b6d4793cfc5404dbaaed2
yangjj <1542103461@qq.com>
2020-01-10 13:31:32 +0800

    feat: 更新配置

--- config.js
+++ config.js
@@ -20,9 +20,9 @@ let ENVIRONMENT = 'dev'; // 环境
 // if (process.env.NODE_BUILD !== 'online') {
 CDN_JS = [...CDN_JS, ...[
     'https://lib.baomitu.com/vue/2.6.10/vue.min.js', // cdn
-    'https://lib.baomitu.com/vue-router/3.0.2/vue-router.min.js',
-    'https://lib.baomitu.com/vuex/3.1.0/vuex.min.js',
-    'https://cdn.bootcss.com/element-ui/2.10.1/index.js',
+    'https://lib.baomitu.com/vue-router/3.1.3/vue-router.min.js',
+    'https://lib.baomitu.com/vuex/3.1.1/vuex.min.js',
+    'https://cdn.bootcss.com/element-ui/2.12.1/index.js',
     'https://lib.baomitu.com/echarts/4.2.1-rc1/echarts-en.common.min.js',
     'https://lib.baomitu.com/jquery/3.3.1/jquery.min.js',
     'https://cdn.bootcss.com/tinymce/4.7.5/tinymce.min.js'
